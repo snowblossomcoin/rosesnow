7ec7fa3ea9d512b0c6a14d27b4377e363c8a47fe
fireduck64 <fireduck@gmail.com>
2020-12-03 22:56:05 -0800

    Add mainnet check items

---
+++ cli/check-const-mainnet.sh
@@ -0,0 +1,7 @@
+
+export RECIPIENT='"snow:h5v2umg4j7h2ayg46mzetjmdpxn54k9v8g24ug5g"'
+
+mkdir -p /data/snowblossom/cli-mainnet
+
+rosetta-cli --configuration-file snowblossom-config-mainnet.json check:construction
+
